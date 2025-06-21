ba56fcd5f3afed98370fe48c1f76a236ea90b9d6
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-21 20:12:14 +0530

    created

--- output.tf
+++ output.tf
@@ -2,3 +2,10 @@
     value = module.vpc.azs_info
   
 } */
+
+/* 
+
+output "subnets_info" {
+  value = module.vpc.subnets_info
+
+} */
--- vpc.tf
+++ vpc.tf
@@ -8,4 +8,7 @@ module "vpc" {
   public_subnet_cidr   = var.public_subnet_cidr
   private_subnet_cidr  = var.private_subnet_cidr
   database_subnet_cidr = var.database_subnet_cidr
+  is_peering_required  = true
+
+  
 }
